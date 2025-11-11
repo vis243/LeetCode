e52e916df1392aa748594ac8acc44d97229dd66b
Vishnu V Bharadwaj <124525871+vis243@users.noreply.github.com>
2025-11-11 23:41:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (10.40%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.java
+++ 1635-number-of-good-pairs/number-of-good-pairs.java
@@ -1,11 +1,11 @@
 class Solution {
     public int numIdenticalPairs(int[] nums) {
         int c=0;
-        int[] count = new int[102];
+        int[] count = new int[101];
         for(int n:nums){
             count[n]++;
         }
-        for(int i=0;i<102;i++){
+        for(int i=0;i<101;i++){
 c+=((count[i])*(count[i]-1))/2;
         }
         return c;
